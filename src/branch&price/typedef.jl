037a42e415cd728da535cb0f368ed23d6d9f83e2
Ayse Arslan <arslan.aysenur@gmail.com>
2022-07-20 14:37:37 +0200

    first round of results on large instances added

--- src/branch&price/typedef.jl
+++ src/branch&price/typedef.jl
@@ -101,11 +101,11 @@ mutable struct BP_params
   restart_for_IP::Bool
   branch_on_vertex::Bool
 
-  function BP_params(_optimizer::String = "GLPK-Cbc", _verbose::Bool = true, _is_pief = false, _fvs = true,  _reduce_vertices = true, _is_column_disjoint = true, _max_intersecting_columns = 6, _is_tabu_list = true, _solve_master_IP = true, _time_limit_IP = 10.0,  _freq_solve_master_IP = 2, _restart_for_IP = true, _branch_on_vertex = false)
+  function BP_params(_optimizer::String = "GLPK-Cbc", _verbose::Bool = true, _is_pief = false, _fvs = true,  _reduce_vertices = true, _is_column_disjoint = true, _max_intersecting_columns = 6, _is_tabu_list = true, _solve_master_IP = true, _time_limit_IP = 30.0,  _freq_solve_master_IP = 2, _restart_for_IP = true, _branch_on_vertex = false)
     return new(_optimizer, _verbose, _is_pief, _fvs, _reduce_vertices, _is_column_disjoint, _max_intersecting_columns, _is_tabu_list, _solve_master_IP, _time_limit_IP, _freq_solve_master_IP, _restart_for_IP, _branch_on_vertex)
   end
   function BP_params(_is_pief::Bool, _verbose::Bool = false)
-    return new("GLPK-Cbc", _verbose, _is_pief, true, true, true, 6, true, true, 10.0, 2, true, false)
+    return new("GLPK-Cbc", _verbose, _is_pief, true, true, true, 6, true, true, 30.0, 2, true, false)
   end
 end
 
