bfa40595526c4166a8e30b402f8e0602ac491ea4
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2023-08-07 11:39:23 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,26 +2,24 @@ using Documenter
 using KidneyExchange
 using DocumenterCitations
 
-bib = CitationBibliography(joinpath(@__DIR__, "references.bib"))
+bib = CitationBibliography(joinpath(@__DIR__, "references.bib"), style = :authoryear)
 
 makedocs(
     bib,
     sitename = "KidneyExchange.jl",
     strict = true,
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true"
-    ),
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     modules = [KidneyExchange],
     pages = [
-        "Home"       => "index.md",
-        "Types"      => "types.md",
-        "Functions"  => "functions.md",
-        "References" => "references.md"
-    ]
+        "Home" => "index.md",
+        "Types" => "types.md",
+        "Functions" => "functions.md",
+        "References" => "references.md",
+    ],
 )
 
 deploydocs(;
     branch = "gh-pages",
     devbranch = "master",
-    repo   = "github.com/jeremyomer/KidneyExchange.jl"
+    repo = "github.com/jeremyomer/KidneyExchange.jl",
 )
